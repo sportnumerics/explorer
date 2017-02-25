a85c0136bb6959d04954b78a6ca5b2d03b2e3522
Will James <wjames@thoughtworks.com>
2017-02-25 15:45:54 +1100

    Setting up for CI

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 set -ex
 
+./decrypt.sh
+source ./env/env.sh
+unset AWS_SESSION_TOKEN
+
+pip install --upgrade awscli
+
 REGION="ap-southeast-2"
 APP_NAME="sportnumerics-explorer"
 # CLOUDFRONT_ID="E38ON9IJP6XEDD"
