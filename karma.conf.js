61e4d302b389d18559e60d38a17c1891324d0fca
Will James <wjames@thoughtworks.com>
2017-03-05 19:22:43 +1100

    Remove phantomjs because why? This stencil sucks. Want to rewrite this whole thing in elm from scratch.

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,6 @@ process.env.NODE_ENV = 'test';
 module.exports = function(config) {
   config.set({
     basePath: '',
-    browsers: [ 'PhantomJS' ],
     files: [
       'test/loadtests.js'
     ],
