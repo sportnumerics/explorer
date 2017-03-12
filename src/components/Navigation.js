6dbdcc5cff2eadbdb37860e888af978d6352ceaf
Will James <wjames@thoughtworks.com>
2017-03-13 02:27:24 +1100

    Fix dropdown and menu hiding issues. Needed to pass onSelect to child element.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -26,30 +26,30 @@ const years = [
   }
 ];
 
-const YearMenuItem = ({currentDiv, toYear}) => (
-  <LinkContainer to={`/${toYear}/divs/${currentDiv}`}><MenuItem>{toYear}</MenuItem></LinkContainer>
+const YearMenuItem = ({currentDiv, toYear, onSelect}) => (
+  <LinkContainer to={`/${toYear}/divs/${currentDiv}`} onSelect={onSelect}><MenuItem>{toYear}</MenuItem></LinkContainer>
 )
 
-const DivLink = ({currentYear, toDiv}) => (
-  <LinkContainer to={`/${currentYear}/divs/${toDiv.id}`}><NavItem>{toDiv.name}</NavItem></LinkContainer>
+const DivLink = ({currentYear, toDiv, onSelect}) => (
+  <LinkContainer to={`/${currentYear}/divs/${toDiv.id}`} onSelect={onSelect}><NavItem>{toDiv.name}</NavItem></LinkContainer>
 )
 
 const Navigation = ({params}) => {
   return (
     <Navbar inverse fixedTop collapseOnSelect>
       <Navbar.Header>
-          <Navbar.Brand>
-            S#
-          </Navbar.Brand>
-          <Navbar.Toggle />
+        <Navbar.Brand>
+          S#
+        </Navbar.Brand>
+        <Navbar.Toggle />
       </Navbar.Header>
       <Navbar.Collapse>
-        <Nav>
-          <NavDropdown title='Years' id='years-dropdown'>
-            { years.map(year => <YearMenuItem key={year.id} currentDiv={params.div} toYear={year.id} />) }
-          </NavDropdown>
-          { divs.map(div => <DivLink key={div.id} currentYear={params.year} toDiv={div} />) }
-        </Nav>
+      <Nav>
+      <NavDropdown title='Years' id='basic-nav-dropdown'>
+        { years.map((year, i) => <YearMenuItem key={i} currentDiv={params.div} toYear={year.id} />) }
+      </NavDropdown>
+      { divs.map((div, i) => <DivLink key={i} currentYear={params.year} toDiv={div} />) }
+      </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
