f03ede24e82fab54d18aa789ed44e5779f88e99d
Will James <wjames@thoughtworks.com>
2017-06-15 22:36:47 +1000

    Remove cloudfront distribution from explorer, since we are doing blue green deployments now

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,15 @@ source ./env/env.sh
 unset AWS_SESSION_TOKEN
 
 if [ "$LAMBCI_BRANCH" = "master" ]; then
-  export STAGE=prod
-  API_STAGE="green"
-  EXPLORER_API_PREFIX="explorer-api-$API_STAGE"
+  STACK_PREFIX="sportnumerics-explorer"
+  STAGE="prodgreen"
+  if aws cloudformation describe-stacks --stack-name "$STACK_PREFIX-$STAGE"; then
+    STAGE="prodblue"
+  fi
+  EXPLORER_API_PREFIX="explorer-api-$STAGE"
 else
-  export STAGE=dev
+  STAGE=dev
   EXPLORER_API_PREFIX="explorer-api.dev"
-  API_STAGE="dev"
 fi
 
 REGION="ap-southeast-2"
@@ -28,10 +30,6 @@ export EXPLORER_API_URL="https://$EXPLORER_API_PREFIX.sportnumerics.com"
 
 npm run dist
 
-aws cloudformation deploy --stack-name $STACK_NAME --parameter-overrides "StageParameter=$STAGE" "ExplorerApiStage=$API_STAGE" --template-file $TEMPLATE_FILE || true
-
-CLOUDFRONT_ID=$(aws cloudformation describe-stacks --stack-name sportnumerics-explorer-$STAGE --query 'Stacks[0].Outputs[?OutputKey==`CloudfrontArn`].OutputValue' --output text)
+aws cloudformation deploy --stack-name $STACK_NAME --parameter-overrides "StageParameter=$STAGE" --template-file $TEMPLATE_FILE || true
 
 aws s3 sync dist "s3://$BUCKET_NAME" --delete
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths / /logo-196.png /logo-180.png /favicon.ico
