80b6d621fba00e18abeb8829a566340082210b48
Will James <wjames@thoughtworks.com>
2017-06-15 21:36:50 +1000

    Reference green explorer api deployment in prod... not ready to deploy this yet

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,12 @@ unset AWS_SESSION_TOKEN
 
 if [ "$LAMBCI_BRANCH" = "master" ]; then
   export STAGE=prod
-  EXPLORER_API_PREFIX="explorer-api"
+  API_STAGE="green"
+  EXPLORER_API_PREFIX="explorer-api-$API_STAGE"
 else
   export STAGE=dev
   EXPLORER_API_PREFIX="explorer-api.dev"
+  API_STAGE="dev"
 fi
 
 REGION="ap-southeast-2"
@@ -27,7 +29,7 @@ export EXPLORER_API_URL="https://$EXPLORER_API_PREFIX.sportnumerics.com"
 
 npm run dist
 
-aws cloudformation deploy --stack-name $STACK_NAME --parameter-overrides "StageParameter=$STAGE" --template-file $TEMPLATE_FILE || true
+aws cloudformation deploy --stack-name $STACK_NAME --parameter-overrides "StageParameter=$STAGE" "ExplorerApiStage=$API_STAGE" --template-file $TEMPLATE_FILE || true
 
 CLOUDFRONT_ID=$(aws cloudformation describe-stacks --stack-name sportnumerics-explorer-$STAGE --query 'Stacks[0].Outputs[?OutputKey==`CloudfrontArn`].OutputValue' --output text)
 
