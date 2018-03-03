2962005248706635d40f4fa06474f1b8ddfb09f8
Will James <wjames@thoughtworks.com>
2018-03-02 23:59:30 -0600

    Fix some issues for building

--- cfg/dist.js
+++ cfg/dist.js
@@ -6,22 +6,19 @@ let webpack = require('webpack');
 let baseConfig = require('./base');
 let defaultSettings = require('./defaults');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
-let HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 let config = Object.assign({}, baseConfig, {
   entry: path.join(__dirname, '../src/index'),
   cache: false,
   devtool: 'sourcemap',
+  optimization: {
+    minimize: true
+  },
   plugins: [
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'production',
       EXPLORER_API_URL: process.env.EXPLORER_API_URL,
       STAGE: process.env.APP_STAGE
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        inline_script: true
-      }
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
@@ -30,8 +27,7 @@ let config = Object.assign({}, baseConfig, {
       filename: 'index.html',
       template: path.join(__dirname, '../src/index.html'),
       inlineSource: '.(js)$'
-    }),
-    new HtmlWebpackInlineSourcePlugin()
+    })
   ],
   module: defaultSettings.getDefaultModules()
 });
