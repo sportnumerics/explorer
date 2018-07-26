098bc63d3c6b9a24687e855ae51b4ec86a0d094f
Will James <wjames@thoughtworks.com>
2018-07-25 21:58:08 -0500

    Fix app stage

--- src/config/dist.js
+++ src/config/dist.js
@@ -4,14 +4,14 @@ import baseConfig from './base';
 
 const EXPLORER_API_URL = process.env.EXPLORER_API_URL;
 const GIT_SHA = process.env.GIT_SHA;
-const APP_STAGE = process.env.APP_STAGE;
+const STAGE = process.env.STAGE;
 const NODE_ENV = process.env.NODE_ENV;
 
 let config = {
   appEnv: 'dist',  // feel free to remove the appEnv property here
   apiUrl: EXPLORER_API_URL,
   gitSha: GIT_SHA,
-  appStage: APP_STAGE,
+  appStage: STAGE,
   nodeEnv: NODE_ENV
 };
 
