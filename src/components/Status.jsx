11e6ed5c653e175fccf8d79c57008c2e57132257
Will James <wjames@thoughtworks.com>
2018-07-25 21:49:25 -0500

    Add github link to status

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -8,7 +8,7 @@ const Status = () => (
       <Row>
         <Col xs={12} sm={8} smOffset={2}>
           <ListGroup>
-            <ListGroupItem>git sha <Badge>{ config.gitSha }</Badge></ListGroupItem>
+            <ListGroupItem>git sha <GithubLink sha={config.gitSha}><Badge>{ config.gitSha }</Badge></GithubLink></ListGroupItem>
             <ListGroupItem>api url <Label bsStyle={ /blue/.test(config.apiUrl) ? 'primary' : 'success' }>{ config.apiUrl }</Label></ListGroupItem>
             <ListGroupItem>app stage <Badge>{ config.appStage }</Badge></ListGroupItem>
             <ListGroupItem>node env <Badge>{ config.appStage }</Badge></ListGroupItem>
@@ -18,5 +18,13 @@ const Status = () => (
     </Grid>
   </div>
 )
+
+const GithubLink = ({sha, children}) => {
+  if (/\w{40}/.test(sha)) {
+    return <a href={`https://github.com/sportnumerics/explorer/commit/${config.gitSha}`}>{children}</a>
+  } else {
+    return <span>{children}</span>
+  }
+}
 
 export default Status;
