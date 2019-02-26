68e799845da4c8e44c47242437944b927c0d239d
Will James <jameswt@gmail.com>
2019-02-25 21:38:19 -0600

    Switch twitter link to sportnumerics

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => <div className="footer">
     <div className="container">
-        <p className="align-middle"><a href="https://twitter.com/wiggzz"><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a></p>
+        <p className="align-middle"><a href="https://twitter.com/sportnumerics"><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a></p>
     </div>
 </div>
