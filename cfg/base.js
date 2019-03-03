69091b2053e88b189c25f056ca7569458cc33fbf
Will James <jameswt@gmail.com>
2019-03-03 09:13:47 -0600

    Use hash in app bundle to cache bust. May also need to adjust cloudfront properties

--- cfg/base.js
+++ cfg/base.js
@@ -13,7 +13,7 @@ module.exports = {
   devtool: 'eval',
   output: {
     path: path.join(__dirname, '/../dist'),
-    filename: 'app.js',
+    filename: 'app.[hash].js',
     publicPath: defaultSettings.publicPath
   },
   devServer: {
@@ -33,7 +33,8 @@ module.exports = {
       sources: `${defaultSettings.srcPath}/sources/`,
       stores: `${defaultSettings.srcPath}/stores/`,
       styles: `${defaultSettings.srcPath}/styles/`,
-      config: `${defaultSettings.srcPath}/config/` + process.env.REACT_WEBPACK_ENV
+      config:
+        `${defaultSettings.srcPath}/config/` + process.env.REACT_WEBPACK_ENV
     }
   },
   module: {}
