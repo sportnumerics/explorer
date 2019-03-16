a381698f18202b987a7a7d9ec9caae666aaa2612
Will James <jameswt@gmail.com>
2019-03-15 21:47:29 -0500

    Show games list and link to teams

--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -2,43 +2,28 @@ import React from 'react'
 import GameDate from './GameDate'
 import GameResult from './GameResult'
 import { Link } from 'react-router-dom'
-import { round } from '../utils/utils'
 import { Table } from 'react-bootstrap'
 
 const GamesList = ({ games, year }) => {
   return (
     <Table>
       <thead>
-        <tr><th>Date</th><th>Home</th><th>Away</th><th>Result</th><th>Prediction</th><th>Error</th></tr>
+        <tr><th>Date</th><th>Home</th><th>Away</th><th>Result</th><th>Prediction</th></tr>
       </thead>
       <tbody>
         {games.map((game, index) => {
           return (
             <tr key={index}>
               <td><GameDate iso8601dateString={game.date} /></td>
-              <td>{ game.team.name }</td>
+              <td><Link to={`/${year}/teams/${game.team.id}`}>{ game.team.name }</Link></td>
               <td>{ !game.opponent.nonDivisional ? (<Link to={`/${year}/teams/${game.opponent.id}`}>{game.opponent.name}</Link>) : game.opponent.name }</td>
               <td>{game.result && (<GameResult pointsFor={game.result.pointsFor} pointsAgainst={game.result.pointsAgainst} />) }</td>
               <td>{game.predictions && (<GameResult unimportant={game.result} pointsFor={game.predictions.llsGoalsFor} pointsAgainst={game.predictions.llsGoalsAgainst} />) }</td>
-              <td><GameError game={game}/></td>
             </tr>
         )})}
       </tbody>
     </Table>
   );
-}
-
-const GameError = ({game}) => {
-  if (game.result && game.predictions) {
-    const llsError = game.result.pointsFor - game.predictions.llsGoalsFor - game.result.pointsAgainst + game.predictions.llsGoalsAgainst;
-    if (llsError >= 0) {
-      return <span className="positive-error">{ round(llsError, 2) }</span>
-    } else {
-      return <span className="negative-error">{ round(llsError, 2) }</span>
-    }
-  } else {
-    return null;
-  }
 }
 
 export default GamesList
