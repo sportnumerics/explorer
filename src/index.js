54df983264583313aa0e7d35e0d31b59ea1e33d0
Will James <jameswt@gmail.com>
2023-05-08 09:44:49 -0500

    Fix google analytics

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
-import history from './services/history';
-import ReactGA from 'react-ga';
-import config from 'config';
-
-ReactGA.initialize(config.gaTrackingId, config.gaOptions);
-
-history.listen(location => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
-});
 
 // Render the main component into the dom
 ReactDOM.render(<Root />, document.getElementById('app'));
